955a123d1d13b319a57abb9a7d1db39d38c141be
Aleksandr <sas0501@yandex.ru>
2023-05-21 19:19:16 +0300

    ТЗ9

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,28 +1,15 @@
 package ru.yandex.practicum.filmorate;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.filmorate.model.User;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.LocalDate;
-
 
 @SpringBootTest
 class FilmorateApplicationTests {
 
 
-	@Test
-	void contextLoads() throws IOException, InterruptedException {
+    @Test
+    void contextLoads() {
 
-	}
+    }
 
 }
